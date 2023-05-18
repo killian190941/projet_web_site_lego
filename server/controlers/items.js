ecb89074929311a9b50ba3ecf8020cdb244039a7
Marco Villanueva <marco.villanueva.legler@gmail.com>
2023-05-18 18:08:51 +0200

    added filter for quotation marks

--- server/controlers/items.js
+++ server/controlers/items.js
@@ -5,6 +5,9 @@ module.exports = {
     async add({params, model, HTTPError, user, config}) { /* nom du chemin cree dans publish.js dans la requète url; url:"/items/publish", */
         if (params.title == "") throw new HTTPError("Il faut un titre.", 406);
         if (params.description == "") throw new HTTPError("Il faut une description.", 406);
+        if (params.title.includes('"') || params.description.includes('"')) {
+            throw new HTTPError("Pas possible de mettre des guillemets dans le titre ou la description.", 406);
+        }
         if(params.file.length!=1)throw new HTTPError('Veuillez ajouter un fichier.', 444);
         if (!/\.(jpeg|png|jpg)$/i.test(params.file[0].name)) throw new HTTPError("Le format du fichier doit être .jpg, .jpeg ou .png",400);
         let filename=path.join(config.uploadDirectory,params.file[0].name);
--- server/controlers/monProfil.js
+++ server/controlers/monProfil.js
@@ -4,16 +4,16 @@ const fs=require('fs')
 module.exports={
     listAbos({config, model, user}) {
         let myid = model.users.getByLogin(user.login).id;
-        let myImages = model.latestImages.getOthersImages(myid);
-        for (let i = 0; i < myImages.length; i++) {
-            if (myImages[i].score === null) {
-                myImages[i].score = "non noté";
+        let aboImages = model.latestImages.getOthersImages(myid);
+        for (let i = 0; i < aboImages.length; i++) {
+            if (aboImages[i].score === null) {
+                aboImages[i].score = "non noté";
             } else {
-                myImages[i].score = myImages[i].score.toFixed(2)
+                aboImages[i].score = aboImages[i].score.toFixed(2)
             }
           };
         // Prepare the data to return
-        let data = myImages.map(image => {
+        let data = aboImages.map(image => {
             return {
                 path: image.path,
                 title: image.title,
@@ -63,8 +63,20 @@ module.exports={
         return model.items.delete(params.id, 422);
     }, 
 
-    modification({ model, params}) {
-        return model.items.updateTitle(params.id,params.title),
-        model.items.updateDescription(params.id,params.description);
+modification({ model, params, HTTPError }) {
+    let currentItem = model.items.getById(params.id);
+
+    if (params.title == currentItem.title && params.description == currentItem.description) {
+        throw new HTTPError("Aucune modification n'a été apportée au titre ou à la description.", 406);
+    }
+
+    if (params.title == "") throw new HTTPError("Il faut un titre.", 406);
+    if (params.description == "") throw new HTTPError("Il faut une description.", 406);
+    if (params.title.includes('"') || params.description.includes('"')) {
+        throw new HTTPError("Pas possible de mettre des guillemets dans le titre ou la description.", 406);
+    }
+
+    return model.items.updateTitle(params.id,params.title),
+    model.items.updateDescription(params.id,params.description);
     }
 }
